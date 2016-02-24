e853d60b7ac7c3e2e74fbf7c097ff50136b51603
drioemgaoin <romain_diegoni@hotmail.com>
2016-02-24 22:10:13 +0000

    Faace factory unit tests

---
+++ RubbikCubeDomain/Tests/Factory/FaceFactoryTests.cs
@@ -0,0 +1,87 @@
+﻿using System.Windows.Media;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+using RubbikCubeDomain.Entity;
+using RubbikCubeDomain.Enums;
+using RubbikCubeDomain.Factory;
+
+namespace RubbikCubeDomain.Tests.Factory
+{
+    [TestFixture]
+    public class FaceFactoryTests
+    {
+        private IFixture fixture;
+
+        [OneTimeSetUp]
+        public void TestFixtureSetUp()
+        {
+            fixture = new Fixture();
+        }
+
+        [Test]
+        public void GivenFrontFaceRequest_WhenCreateFace_ShouldReturnFrontFaceInstance()
+        {
+            var subject = fixture.Create<FaceFactory>();
+
+            var result = subject.CreateFace(FaceType.Front);
+
+            Assert.That(result.Type, Is.EqualTo(FaceType.Front));
+            Assert.That(result.Color, Is.EqualTo(Colors.Red));
+        }
+
+        [Test]
+        public void GivenLeftFaceRequest_WhenCreateFace_ShouldReturnLeftFaceInstance()
+        {
+            var subject = fixture.Create<FaceFactory>();
+
+            var result = subject.CreateFace(FaceType.Left);
+
+            Assert.That(result.Type, Is.EqualTo(FaceType.Left));
+            Assert.That(result.Color, Is.EqualTo(Colors.White));
+        }
+
+        [Test]
+        public void GivenRightFaceRequest_WhenCreateFace_ShouldReturnRightFaceInstance()
+        {
+            var subject = fixture.Create<FaceFactory>();
+
+            var result = subject.CreateFace(FaceType.Right);
+
+            Assert.That(result.Type, Is.EqualTo(FaceType.Right));
+            Assert.That(result.Color, Is.EqualTo(Colors.Yellow));
+        }
+
+        [Test]
+        public void GivenTopFaceRequest_WhenCreateFace_ShouldReturnTopFaceInstance()
+        {
+            var subject = fixture.Create<FaceFactory>();
+
+            var result = subject.CreateFace(FaceType.Top);
+
+            Assert.That(result.Type, Is.EqualTo(FaceType.Top));
+            Assert.That(result.Color, Is.EqualTo(Colors.Blue));
+        }
+
+        [Test]
+        public void GivenBottomFaceRequest_WhenCreateFace_ShouldReturnBottomFaceInstance()
+        {
+            var subject = fixture.Create<FaceFactory>();
+
+            var result = subject.CreateFace(FaceType.Bottom);
+
+            Assert.That(result.Type, Is.EqualTo(FaceType.Bottom));
+            Assert.That(result.Color, Is.EqualTo(Colors.Green));
+        }
+
+        [Test]
+        public void GivenBackFaceRequest_WhenCreateFace_ShouldReturnBackFaceInstance()
+        {
+            var subject = fixture.Create<FaceFactory>();
+
+            var result = subject.CreateFace(FaceType.Back);
+
+            Assert.That(result.Type, Is.EqualTo(FaceType.Back));
+            Assert.That(result.Color, Is.EqualTo(Colors.Orange));
+        }
+    }
+}

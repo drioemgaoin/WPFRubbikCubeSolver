f0253a55ccd199ffe98739ff101b261a147a73bc
drioemgaoin <romain.diegoni@hotmail.com>
2016-03-17 11:43:05 +0000

    Move cube logic
    Move the logic cube in the cube itself and not in the cube service

--- RubbikCubeDomain/Entity/Cube.cs
+++ RubbikCubeDomain/Entity/Cube.cs
@@ -1,14 +1,123 @@
-﻿namespace RubiksCube.Entity
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RubiksCube.Enums;
+
+namespace RubiksCube.Entity
 {
     public class Cube
     {
-        public double[,] Matrix { get; set; }
-         
         public Face FrontFace { get; set; }
         public Face LeftFace { get; set; }
         public Face RightFace { get; set; }
         public Face BottomFace { get; set; }
         public Face TopFace { get; set; }
         public Face BackFace { get; set; }
+
+        public IList<Face> RotateOnRightSide(double[,] matrix, RotationType rotationType)
+        {
+            var frontFacies = FrontFace.RotateRow(matrix, rotationType).ToList();
+            var leftFacies = LeftFace.RotateRow(matrix, rotationType).ToList();
+            var rightFacies = RightFace.RotateRow(matrix, rotationType).ToList();
+            var backFacies = BackFace.RotateRow(matrix, rotationType).ToList();
+
+            Move(LeftFace, FrontFace, leftFacies);
+            Move(BackFace, LeftFace, backFacies);
+            Move(RightFace, BackFace, rightFacies);
+            Move(FrontFace, RightFace, frontFacies);
+
+            System.Diagnostics.Debug.WriteLine(this);
+
+            return frontFacies
+                .Union(leftFacies)
+                .Union(rightFacies)
+                .Union(backFacies).ToList();
+        }
+
+        public IList<Face> RotateOnLeftSide(double[,] matrix, RotationType rotationType)
+        {
+            var frontFacies = FrontFace.RotateRow(matrix, rotationType).ToList();
+            var leftFacies = LeftFace.RotateRow(matrix, rotationType).ToList();
+            var rightFacies = RightFace.RotateRow(matrix, rotationType).ToList();
+            var backFacies = BackFace.RotateRow(matrix, rotationType).ToList();
+
+            Move(RightFace, FrontFace, rightFacies);
+            Move(BackFace, RightFace, backFacies);
+            Move(LeftFace, BackFace, leftFacies);
+            Move(FrontFace, LeftFace, frontFacies);
+
+            System.Diagnostics.Debug.WriteLine(this);
+
+            return frontFacies
+                .Union(leftFacies)
+                .Union(rightFacies)
+                .Union(backFacies).ToList();
+        }
+
+        public IList<Face> RotateOnUpSide(double[,] matrix, RotationType rotationType)
+        {
+            var frontFacies = FrontFace.RotateColumn(matrix, rotationType).ToList();
+            var bottomFacies = BottomFace.RotateColumn(matrix, rotationType).ToList();
+            var topFacies = TopFace.RotateColumn(matrix, rotationType).ToList();
+            var backFacies = BackFace.RotateColumn(matrix, rotationType).ToList();
+
+            Move(BottomFace, FrontFace, bottomFacies);
+            Move(BackFace, BottomFace, backFacies);
+            Move(TopFace, BackFace, topFacies);
+            Move(FrontFace, TopFace, frontFacies);
+
+            System.Diagnostics.Debug.WriteLine(this);
+
+            return frontFacies
+                .Union(topFacies)
+                .Union(bottomFacies)
+                .Union(backFacies).ToList();
+        }
+
+        public IList<Face> RotateOnDownSide(double[,] matrix, RotationType rotationType)
+        {
+            var frontFacies = FrontFace.RotateColumn(matrix, rotationType).ToList();
+            var bottomFacies = BottomFace.RotateColumn(matrix, rotationType).ToList();
+            var topFacies = TopFace.RotateColumn(matrix, rotationType).ToList();
+            var backFacies = BackFace.RotateColumn(matrix, rotationType).ToList();
+
+            Move(TopFace, FrontFace, topFacies);
+            Move(BackFace, TopFace, backFacies);
+            Move(BottomFace, BackFace, bottomFacies);
+            Move(FrontFace, BottomFace, frontFacies);
+
+            System.Diagnostics.Debug.WriteLine(this);
+
+            return frontFacies
+                .Union(topFacies)
+                .Union(bottomFacies)
+                .Union(backFacies).ToList();
+        }
+
+        private static void Move(Face source, Face target, IList<Face> facies)
+        {
+            foreach (var facie in facies)
+            {
+                source.Facies.Remove(facie);
+            }
+
+            foreach (var facie in facies)
+            {
+                facie.Move(target.Type);
+                target.Facies.Add(facie);
+            }
+        }
+
+        public override string ToString()
+        {
+            var buffer = new StringBuilder();
+            buffer.Append(FrontFace);
+            buffer.Append(RightFace);
+            buffer.Append(BackFace);
+            buffer.Append(LeftFace);
+            buffer.Append(TopFace);
+            buffer.Append(BottomFace);
+            return buffer.ToString();
+        }
     }
 }
--- RubbikCubeDomain/Entity/Face.cs
+++ RubbikCubeDomain/Entity/Face.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Media;
@@ -26,11 +26,9 @@ namespace RubiksCube.Entity
         {
             get
             {
-                return FaciePositionType.ToString();//String.Format("{0} {1}", Type, FaciePositionType)); 
+                return FaciePositionType.ToString();
             }
         }
-
-        public MoveType Move { get; set; }
 
         public FaceType Type
         {
@@ -54,7 +52,120 @@ namespace RubiksCube.Entity
 
         public Color Color { get; set; }
 
+        public string ColorName
+        {
+            get
+            {
+                if (Color == Colors.Blue)
+                {
+                    return "Blue";
+                }
+
+                if (Color == Colors.Red)
+                {
+                    return "Red";
+                }
+
+                if (Color == Colors.Green)
+                {
+                    return "Green";
+                }
+
+                if (Color == Colors.Orange)
+                {
+                    return "Orange";
+                }
+
+                if (Color == Colors.White)
+                {
+                    return "White";
+                }
+
+                if (Color == Colors.Yellow)
+                {
+                    return "Yellow";
+                }
+
+                return "NA";
+            }
+        }
+
         public IList<Face> Facies { get; set; }
+
+        public IList<Face> RotateRow(double[,] matrix, RotationType rotationType)
+        {
+            var facies = Facies.Where(x => IsRowMatch(x, rotationType)).ToList();
+            Rotate(facies, matrix);
+            return facies;
+        }
+
+        public IList<Face> RotateColumn(double[,] matrix, RotationType rotationType)
+        {
+            var facies = Facies.Where(x => IsColumnMatch(x, rotationType)).ToList();
+            Rotate(facies, matrix);
+            return facies;
+        }
+
+        public void Move(FaceType target)
+        {
+            if ((Type == FaceType.Back && target == FaceType.Top) ||
+                (Type == FaceType.Front && target == FaceType.Bottom) ||
+                (Type == FaceType.Bottom && target == FaceType.Front) ||
+                (Type == FaceType.Top && target == FaceType.Back))
+            {
+                switch (FaciePositionType)
+                {
+                    case FaciePositionType.LeftTop:
+                        FaciePositionType = FaciePositionType.LeftBottom;
+                        break;
+                    case FaciePositionType.LeftBottom:
+                        FaciePositionType = FaciePositionType.LeftTop;
+                        break;
+                    case FaciePositionType.MiddleTop:
+                        FaciePositionType = FaciePositionType.MiddleBottom;
+                        break;
+                    case FaciePositionType.MiddleBottom:
+                        FaciePositionType = FaciePositionType.MiddleTop;
+                        break;
+                    case FaciePositionType.RightTop:
+                        FaciePositionType = FaciePositionType.RightBottom;
+                        break;
+                    case FaciePositionType.RightBottom:
+                        FaciePositionType = FaciePositionType.RightTop;
+                        break;
+                }
+            }
+
+            if ((Type == FaceType.Back && target == FaceType.Left) ||
+                (Type == FaceType.Right && target == FaceType.Back) ||
+                (Type == FaceType.Back && target == FaceType.Right) ||
+                (Type == FaceType.Left && target == FaceType.Back))
+            {
+                switch (FaciePositionType)
+                {
+                    case FaciePositionType.LeftTop:
+                        FaciePositionType = FaciePositionType.RightTop;
+                        break;
+                    case FaciePositionType.RightTop:
+                        FaciePositionType = FaciePositionType.LeftTop;
+                        break;
+                    case FaciePositionType.LeftMiddle:
+                        FaciePositionType = FaciePositionType.RightMiddle;
+                        break;
+                    case FaciePositionType.RightMiddle:
+                        FaciePositionType = FaciePositionType.LeftMiddle;
+                        break;
+                    case FaciePositionType.LeftBottom:
+                        FaciePositionType = FaciePositionType.RightBottom;
+                        break;
+                    case FaciePositionType.RightBottom:
+                        FaciePositionType = FaciePositionType.LeftBottom;
+                        break;
+                }
+            }
+
+            Type = target;
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -67,13 +178,67 @@ namespace RubiksCube.Entity
         public override string ToString()
         {
             var buffer = new StringBuilder();
-            buffer.AppendLine(Type + "-" + faciePositionType + "-" + Color + "=" + Facies.Count);
+            buffer.AppendLine(Type + "-" + faciePositionType + "-" + ColorName + "=" + Facies.Count);
             foreach (var face in Facies)
             {
                 buffer.AppendLine("-->" + face);
             }
 
             return buffer.ToString();
+        }
+
+        private static void Rotate(IEnumerable<Face> facies, double[,] matrix)
+        {
+            foreach (var facie in facies)
+            {
+                facie.Rotation = facie.Rotation == null ? matrix : MatrixHelper.Multiply(facie.Rotation, matrix);
+            }
+        }
+
+        private static bool IsRowMatch(Face facie, RotationType rotationType)
+        {
+            switch (rotationType)
+            {
+                case RotationType.All:
+                    return true;
+                case RotationType.First:
+                    return facie.FaciePositionType == FaciePositionType.LeftTop ||
+                           facie.FaciePositionType == FaciePositionType.MiddleTop ||
+                           facie.FaciePositionType == FaciePositionType.RightTop;
+                case RotationType.Second:
+                    return facie.FaciePositionType == FaciePositionType.LeftMiddle ||
+                           facie.FaciePositionType == FaciePositionType.Middle ||
+                           facie.FaciePositionType == FaciePositionType.RightMiddle;
+                case RotationType.Third:
+                    return facie.FaciePositionType == FaciePositionType.LeftBottom ||
+                           facie.FaciePositionType == FaciePositionType.MiddleBottom ||
+                           facie.FaciePositionType == FaciePositionType.RightBottom;
+            }
+
+            return false;
+        }
+
+        private static bool IsColumnMatch(Face facie, RotationType rotationType)
+        {
+            switch (rotationType)
+            {
+                case RotationType.All:
+                    return true;
+                case RotationType.First:
+                    return facie.FaciePositionType == FaciePositionType.LeftTop ||
+                           facie.FaciePositionType == FaciePositionType.LeftMiddle ||
+                           facie.FaciePositionType == FaciePositionType.LeftBottom;
+                case RotationType.Second:
+                    return facie.FaciePositionType == FaciePositionType.MiddleTop ||
+                           facie.FaciePositionType == FaciePositionType.Middle ||
+                           facie.FaciePositionType == FaciePositionType.MiddleBottom;
+                case RotationType.Third:
+                    return facie.FaciePositionType == FaciePositionType.RightTop ||
+                           facie.FaciePositionType == FaciePositionType.RightMiddle ||
+                           facie.FaciePositionType == FaciePositionType.RightBottom;
+            }
+
+            return false;
         }
     }
 }

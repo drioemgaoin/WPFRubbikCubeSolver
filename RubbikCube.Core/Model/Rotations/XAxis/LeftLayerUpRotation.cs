6d5de744e09c8b58fd4485e1e431d1aedf50ed01
Astrochicken1 <jeromestpierre@gmail.com>
2016-03-28 19:24:13 +0100

    Adding missing rotation classes.

---
+++ RubbikCube.Core/Model/Rotations/XAxis/LeftLayerUpRotation.cs
@@ -0,0 +1,32 @@
+﻿namespace RubiksCube.Core.Model.Rotations.XAxis
+{
+    public class LeftLayerUpRotation : XAxisRotation
+    {
+        public LeftLayerUpRotation(double angle, uint times) : base(angle, times)
+        {
+        }
+
+        protected override LayerType LayerType => LayerType.First;
+
+        protected override void Move(Cube cube, FaceType faceType, Facie facie)
+        {
+            switch (faceType)
+            {
+                case FaceType.Front:
+                    cube[FaceType.Top].Add(facie);
+                    break;
+                case FaceType.Top:
+                    FlipPosition(facie);
+                    cube[FaceType.Back].Add(facie);
+                    break;
+                case FaceType.Back:
+                    cube[FaceType.Bottom].Add(facie);
+                    break;
+                case FaceType.Bottom:
+                    FlipPosition(facie);
+                    cube[FaceType.Front].Add(facie);
+                    break;
+            }
+        }
+    }
+}
---
+++ RubbikCube.Core/Model/Rotations/YAxis/TopLayerRightRotation.cs
@@ -0,0 +1,32 @@
+﻿namespace RubiksCube.Core.Model.Rotations.YAxis
+{
+    public class TopLayerRightRotation : YAxisRotation
+    {
+        public TopLayerRightRotation(double angle, uint times) : base(angle, times)
+        {
+        }
+
+        protected override LayerType LayerType => LayerType.First;
+
+        protected override void Move(Cube cube, FaceType faceType, Facie facie)
+        {
+            switch (faceType)
+            {
+                case FaceType.Front:
+                    cube[FaceType.Right].Add(facie);
+                    break;
+                case FaceType.Right:
+                    FlipPosition(facie);
+                    cube[FaceType.Back].Add(facie);
+                    break;
+                case FaceType.Back:
+                    FlipPosition(facie);
+                    cube[FaceType.Left].Add(facie);
+                    break;
+                case FaceType.Left:
+                    cube[FaceType.Front].Add(facie);
+                    break;
+            }
+        }
+    }
+}

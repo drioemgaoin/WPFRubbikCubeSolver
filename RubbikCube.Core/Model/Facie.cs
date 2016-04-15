d152ee62ce91c782cc243b0b7e827abcb1e576e4
Diegoni <romain_diegoni@hotmail.com>
2016-04-15 17:59:01 +0100

    Movement front and back but need to be designed, actually the algorithm is in raw

--- RubbikCube.Core/Model/Facie.cs
+++ RubbikCube.Core/Model/Facie.cs
@@ -1,15 +1,26 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace RubiksCube.Core.Model
 {
-    public class Facie : ICloneable
+    public class Facie : ICloneable, INotifyPropertyChanged
     {
         // TODO: Encapsulate members and use methods to implement the behaviour changing the state
 
         public string Key { get; set; }
 
-        public FaciePositionType FaciePosition { get; set; }
+        private FaciePositionType faciePosition;
+        public FaciePositionType FaciePosition
+        {
+            get { return faciePosition; }
+            set
+            {
+                faciePosition = value;
+                OnPropertyChanged("FaciePosition");
+            }
+        }
 
         public Color Color { get; set; }
 
@@ -35,6 +46,13 @@ namespace RubiksCube.Core.Model
             buffer.AppendLine(FaciePosition + "-" + Color);
 
             return buffer.ToString();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
--- RubiksCube.UI/RubiksCubeControl.xaml.cs
+++ RubiksCube.UI/RubiksCubeControl.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Media.Media3D;
 using RubiksCube.Core;

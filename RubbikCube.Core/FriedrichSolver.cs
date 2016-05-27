ada579748a094c67552ad2977de3904beff84e28
Astrochicken1 <jeromestpierre@gmail.com>
2016-05-27 01:34:41 +0100

    Adding some white cross algorythms.

--- RubbikCube.Core/FriedrichSolver.cs
+++ RubbikCube.Core/FriedrichSolver.cs
@@ -26,10 +26,23 @@ namespace RubiksCube.Core
         {
             var algorythms = new Dictionary<EdgePosition, string>();
             algorythms.Add(new EdgePosition(FaceType.Left, Color.White, Color.Red, FaciePositionType.MiddleDown), "D");
+            algorythms.Add(new EdgePosition(FaceType.Left, Color.White, Color.Orange, FaciePositionType.MiddleUp), "U'");
+            algorythms.Add(new EdgePosition(FaceType.Left, Color.White, Color.Green, FaciePositionType.RightMiddle), "L F' D F");
+
+            algorythms.Add(new EdgePosition(FaceType.Right, Color.White, Color.Orange, FaciePositionType.MiddleUp), "U");
+            algorythms.Add(new EdgePosition(FaceType.Right, Color.White, Color.Red, FaciePositionType.MiddleDown), "D'");
+            algorythms.Add(new EdgePosition(FaceType.Right, Color.White, Color.Blue, FaciePositionType.LeftMiddle), "R F' U F");
+
             algorythms.Add(new EdgePosition(FaceType.Back, Color.White, Color.Blue, FaciePositionType.RightMiddle), "R2");
-            algorythms.Add(new EdgePosition(FaceType.Up, Color.White, Color.Orange, FaciePositionType.MiddleUp), "U2 U' F R' F'");
+            algorythms.Add(new EdgePosition(FaceType.Back, Color.White, Color.Orange, FaciePositionType.MiddleUp), "U2");
+
+            algorythms.Add(new EdgePosition(FaceType.Down, Color.White, Color.Green, FaciePositionType.RightMiddle), "L'");
+            algorythms.Add(new EdgePosition(FaceType.Down, Color.White, Color.Blue, FaciePositionType.LeftMiddle), "R");
+
+            algorythms.Add(new EdgePosition(FaceType.Up, Color.White, Color.Green, FaciePositionType.LeftMiddle), "L");
+            algorythms.Add(new EdgePosition(FaceType.Up, Color.White, Color.Blue, FaciePositionType.RightMiddle), "R'");
             algorythms.Add(new EdgePosition(FaceType.Up, Color.White, Color.Orange, FaciePositionType.MiddleDown), "U' F R' F'");
-            algorythms.Add(new EdgePosition(FaceType.Up, Color.White, Color.Green, FaciePositionType.MiddleUp), "B L2' L' X X L");
+            algorythms.Add(new EdgePosition(FaceType.Up, Color.White, Color.Orange, FaciePositionType.MiddleUp), "U F R' F'");
 
             var edgePositions = GetEdgePositions(cube, Color.White, new []{FaceType.Back, FaceType.Down, FaceType.Left, FaceType.Right, FaceType.Up});
             foreach (var edgePosition in edgePositions)
